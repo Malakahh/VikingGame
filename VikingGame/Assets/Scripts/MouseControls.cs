ef1c011b8c90ccad69b9f0b7013ccfdded9a45d6
Malakahh <Malakahh@users.noreply.github.com>
2015-08-12 18:02:03 +0200

    Reworked hover to only use one SpriteRenderer. Renamed Overlay SpriteRenderer to FogOfWarRenderer

---
+++ VikingGame/Assets/Scripts/MouseControls.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControls : MonoBehaviour {
+    public GameObject HoverHighlight;
+
+    int worldMapLayer = -1;
+    RaycastHit2D hit;
+
+    void Start()
+    {
+        worldMapLayer = LayerMask.NameToLayer("WorldMap");
+    }
+
+    void Update()
+    {
+        MouseOverHighlight();
+        Visit();
+    }
+
+    void MouseOverHighlight()
+    {
+        hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+            HoverHighlight.transform.position = hit.collider.transform.position - Vector3.forward;
+        }
+        else
+        {
+            HoverHighlight.transform.position = Vector3.forward;
+        }
+    }
+
+    void Visit()
+    {
+        if (Input.GetMouseButton(0) && hit.collider != null)
+        {
+            WorldMapHexagonTile tile = hit.collider.GetComponent<WorldMapHexagonTile>();
+            if (tile != null && !tile.FogOfWar && !tile.Visited)
+            {
+                tile.Visited = true;
+            }
+        }
+    }
+}
